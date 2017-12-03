0349836ea70b549094d9e48ec9b1c3f56704c04f
Unknown <nuno.freitas96@gmail.com>
2017-12-03 16:49:30 +0000

    Progress on part 3, not working currently

--- src/behaviours/PatientBehaviour.java
+++ src/behaviours/PatientBehaviour.java
@@ -2,45 +2,61 @@ package behaviours;
 
 import agents.Patient;
 import jade.core.AID;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.CyclicBehaviour;
 import jade.lang.acl.ACLMessage;
 
 import java.util.LinkedList;
 
-public class PatientBehaviour extends CyclicBehaviour {
+public class PatientBehaviour extends Behaviour {
     Patient p;
+    int step = 0;
     public PatientBehaviour(Patient p){
         this.p = p;
     }
 
     @Override
     public void action() {
-        ACLMessage msg= myAgent.receive();
+        switch(step){
+            case 0:
+                ACLMessage msg= myAgent.receive();
 
-        if (msg != null) {
-            // Message received. Process it
-            String msgContents = msg.getContent();
-            ACLMessage reply = msg.createReply();
-            System.out.println("Agent agents.Patient " + myAgent.getName() + " received " + msgContents);
+                if (msg != null) {
+                    // Message received. Process it
+                    String msgContents = msg.getContent();
+                    ACLMessage reply = msg.createReply();
+                    System.out.println("Agent agents.Patient " + myAgent.getName() + " received " + msgContents);
 
-            if(p.getAvailability()) {
+                    if(p.getAvailability()) {
 
-                 p.addResourceUse(msg.getSender());
-                reply.setPerformative(ACLMessage.AGREE);
-                reply.setContent("I am ready!");
+                         p.addResourceUse(msg.getSender());
+                        reply.setPerformative(ACLMessage.AGREE);
+                        reply.setContent("I am ready!");
 
 
-            }else{
+                    }else{
 
-                reply.setPerformative(ACLMessage.REFUSE);
-                reply.setContent("I am not ready...");
+                        reply.setPerformative(ACLMessage.REFUSE);
+                        reply.setContent("I am not ready...");
 
-            }
-            myAgent.send(reply);
+                    }
+                    myAgent.send(reply);
+
+                }
+                else{
+                   block();
+                }
+            break;
+            case 1:
+
+            break;
         }
-        else{
-           block();
-        }
+    }
+
+    @Override
+    public boolean done() {
+
+        return false;
     }
 
 
--- src/behaviours/ResourceBehaviour.java
+++ src/behaviours/ResourceBehaviour.java
@@ -1,14 +1,22 @@
 package behaviours;
 
 import agents.Resource;
+import jade.core.AID;
+import jade.core.behaviours.Behaviour;
 import jade.core.behaviours.SimpleBehaviour;
 import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
 
-public class ResourceBehaviour extends SimpleBehaviour{
+import java.util.ArrayList;
+
+public class ResourceBehaviour extends Behaviour {
     private int t = 0;
 
     Resource r;
-
+    int step = 0;
+    int getAllProposals = r.getAllPatients().size();
+    ArrayList<AID> allPreparedPatients = new ArrayList<>();
+    private MessageTemplate mt;
     public ResourceBehaviour(Resource r){
 
         this.r = r;
@@ -16,23 +24,58 @@ public class ResourceBehaviour extends SimpleBehaviour{
 
     @Override
     public void action() {
-        ACLMessage msg = new ACLMessage(ACLMessage.INFORM);
-        msg.setLanguage("English");
-        msg.setContent("When can you come?");
-        msg.setPerformative(ACLMessage.PROPOSE);
-        for(int i = 0; i < r.getAllPatients().size(); i++){
+        switch(step){
+            case 0:
+                ACLMessage msg = new ACLMessage(ACLMessage.PROPOSE);
+                msg.setLanguage("English");
+                msg.setContent("When can you come?");
+                msg.setPerformative(ACLMessage.PROPOSE);
+                msg.setConversationId("licitation-process1-" + myAgent.getLocalName());
+                for(int i = 0; i < r.getAllPatients().size(); i++){
+                    msg.addReceiver( r.getAllPatients().get(i));
+                }
+                myAgent.send(msg);
+                System.out.println(myAgent.getName() + " sent a message");
+                mt = MessageTemplate.MatchConversationId("licitation-process1-" + myAgent.getLocalName());
+                step = 1;
+                break;
+            case 1:
+                if(getAllProposals == 0){
+                    step = 2;
+                }
+                else{
+                    ACLMessage reply = myAgent.receive(mt);
+                    if(reply != null){
+                        if(reply.getPerformative() == ACLMessage.AGREE && r.getAllPatients().contains(reply.getSender())){
+                            getAllProposals--;
+                            allPreparedPatients.add(reply.getSender());
 
-        msg.addReceiver( r.getAllPatients().get(i));
-
+                        }else if(reply.getPerformative() == ACLMessage.REFUSE && r.getAllPatients().contains(reply.getSender())){
+                            getAllProposals--;
+                        }
+                    }else{
+                        block();
+                    }
+                }
+                break;
+            case 2:
+                ACLMessage msg2 = new ACLMessage(ACLMessage.CFP);
+                msg2.setLanguage("English");
+                msg2.setContent("What is your priority?");
+                msg2.setConversationId("licitation-process2-" + myAgent.getLocalName());
+                for(int i = 0; i < allPreparedPatients.size(); i++){
+                    msg2.addReceiver( allPreparedPatients.get(i));
+                }
+                myAgent.send(msg2);
+                mt = MessageTemplate.MatchConversationId("licitation-process2-" + myAgent.getLocalName());
+                step = 3;
+                break;
         }
-        myAgent.send(msg);
-        System.out.println(myAgent.getName() + " sent a message");
-
     }
 
     @Override
     public boolean done() {
 
-        return true;
+        return step==3;
     }
 }
